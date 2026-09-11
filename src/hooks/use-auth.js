122586423c0422bd26cbae70c0b26a65e16a16a5
Anupama Kittur <149547951+AK577427@users.noreply.github.com>
2026-09-11 22:55:14 +1000

    Fix import path for AuthContext

--- src/hooks/use-auth.js
+++ src/hooks/use-auth.js
@@ -1,6 +1,6 @@
 import { useContext } from "react";
 
-import { AuthContext } from "../components/AuthProvider";
+import { AuthContext } from "../context/AuthContext";
 
 export const useAuth = () => {
   // We pass in the context and create a custom hook that returns the context auth and setAuth
